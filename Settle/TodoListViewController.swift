21bcefb752ee006448ae0fdd7e5e95d02f1988ea
Raditia Madya <gustiraditia@gmail.com>
2019-06-16 22:50:04 +0700

    Initial commit

---
+++ Settle/TodoListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TodoListViewController.swift
+//  Settle
+//
+//  Created by Raditia Madya on 16/06/19.
+//  Copyright © 2019 Raditia Madya. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+	
+	let itemArray = ["To do 1", "To Do 2", "Todo 3"]
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	//MARK - TableView DataSOurce Methods
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+		
+		cell.textLabel?.text = itemArray[indexPath.row]
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		return itemArray.count
+	}
+	
+	//MARK - TableView Delegate Methods
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+//		tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+		
+		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .none
+		}
+		else {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+		}
+		
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
+	
+
+
+}
+
