58692d652de875b0ab13dac0dd19c06ccc15cdf1
Raditia Madya <gustiraditia@gmail.com>
2019-06-16 20:27:41 +0700

    Initial Commit

---
+++ Settle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settle
+//
+//  Created by Raditia Madya on 16/06/19.
+//  Copyright © 2019 Raditia Madya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
